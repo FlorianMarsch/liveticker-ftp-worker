8174faacdf6d27e884d1587a4804ae60b4316d98
Florian <dekontaminator@googlemail.com>
2016-05-15 02:13:20 +0200

    add feedmonster as new datasource for player information

---
+++ src/main/java/de/florianmarsch/playerdata/dataprovider/FeedmonsterPlayer.java
@@ -0,0 +1,71 @@
+package de.florianmarsch.playerdata.dataprovider;
+
+public class FeedmonsterPlayer {
+	private String country;
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String name;
+	private String position;
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FeedmonsterPlayer other = (FeedmonsterPlayer) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
