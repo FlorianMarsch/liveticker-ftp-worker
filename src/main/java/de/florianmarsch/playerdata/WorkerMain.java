96f47a1f264e05743553f5addcfdbd8039dd07c5
Florian <dekontaminator@googlemail.com>
2016-05-15 03:56:12 +0200

    use new data providers to update player data

--- src/main/java/de/florianmarsch/playerdata/WorkerMain.java
+++ src/main/java/de/florianmarsch/playerdata/WorkerMain.java
@@ -1,14 +1,12 @@
 package de.florianmarsch.playerdata;
 
-import java.text.Normalizer;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.florianmarsch.playerdata.axis.PlayerItem;
-import de.florianmarsch.playerdata.axis.PlayerItemWebservice;
-import de.florianmarsch.playerdata.player.Player;
+import de.florianmarsch.playerdata.dataprovider.DataService;
+import de.florianmarsch.playerdata.dataprovider.Player;
 import de.florianmarsch.playerdata.player.PlayerService;
 
 public class WorkerMain {
@@ -19,19 +17,11 @@ public class WorkerMain {
 	public static void main(String[] args) throws Exception {
 		logger.info("start player update");
 		PlayerService playerService = new PlayerService();
-		PlayerItemWebservice webService = new PlayerItemWebservice();
-		List<PlayerItem> allPlayerItems = webService.getAllPlayers();
-		logger.info("recive "+allPlayerItems.size()+" player info");
-		for (PlayerItem playerItem : allPlayerItems) {
-			Player tempPlayer = new Player();
-			tempPlayer.setExternID(String.valueOf(playerItem.getId()));
-			tempPlayer.setName(playerItem.getName());
-
-			String norm = Normalizer.normalize(playerItem.getName(), Normalizer.Form.NFD);
-			norm = norm.replaceAll("[^\\p{ASCII}]", "");
-			tempPlayer.setNormalizedName(norm);
-
-			playerService.save(tempPlayer);
+		DataService dataService = new DataService();
+		List<Player> allPlayers = dataService.getAllPlayers();
+		logger.info("recive "+allPlayers.size()+" player info");
+		for (Player player : allPlayers) {
+			playerService.save(player);
 		}
 		logger.info("end player update");
 		
--- src/main/java/de/florianmarsch/playerdata/player/PlayerService.java
+++ src/main/java/de/florianmarsch/playerdata/player/PlayerService.java
@@ -19,68 +19,93 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.florianmarsch.playerdata.WorkerMain;
+import de.florianmarsch.playerdata.dataprovider.Player;
 
-public class PlayerService  {
+public class PlayerService {
 
 	final static Logger logger = LoggerFactory.getLogger(WorkerMain.class);
 
-	
 	Map<String, Player> map = new HashMap<String, Player>();
-	
-	public PlayerService(){
+
+	public PlayerService() {
 		try {
 			String all = loadFile("http://fussballmanager.herokuapp.com/Player");
 			JSONObject data = new JSONObject(all);
 			JSONArray players = data.getJSONArray("data");
 			for (int i = 0; i < players.length(); i++) {
 				JSONObject player = players.getJSONObject(i);
-				
+
 				Player tempPlayer = new Player();
-				tempPlayer.setExternID(player.getString("externID"));
 				tempPlayer.setId(player.getString("id"));
+				tempPlayer.setComunio(player.getString("comunio"));
+				tempPlayer.setFeedmonster(player.getString("feedmonster"));
+				tempPlayer.setCountry(player.getString("country"));
+				tempPlayer.setFirstName(player.getString("firstName"));
+				tempPlayer.setLastName(player.getString("lastName"));
 				tempPlayer.setName(player.getString("name"));
-				tempPlayer.setNormalizedName(player.getString("normalizedName"));
-				map.put(tempPlayer.getExternID(), tempPlayer);
+				tempPlayer.setAbbreviationName(player.getString("abbreviationName"));
+				tempPlayer.setPosition(player.getString("position"));
+				tempPlayer.setAge(player.getString("age"));
+				tempPlayer.setThumbnail(player.getString("thumbnail"));
+
+				map.put(tempPlayer.getFeedmonster(), tempPlayer);
 			}
-			
-			
+
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		logger.info("already "+map.size()+" players here");
-		
-	
+		logger.info("already " + map.size() + " players here");
+
 	}
-	
+
 	public void save(Player aPlayer) throws IOException, JSONException {
-		String externID = aPlayer.getExternID();
-		Player tPlayer = map.get(externID);
-		if(tPlayer == null){
-			
-			logger.info("new player detected : "+aPlayer.getNormalizedName());
-			
-			
-			JSONObject tempJSONObject = new JSONObject();
-			
-			tempJSONObject.put("externID", aPlayer.getExternID());
-			tempJSONObject.put("name", aPlayer.getName());
-			tempJSONObject.put("normalizedName", aPlayer.getNormalizedName());
-			
-			String json = tempJSONObject .toString();
-			URL url = new URL("http://fussballmanager.herokuapp.com/Player/new");
-			HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-			httpCon.setDoOutput(true);
-			httpCon.setRequestMethod("PUT");
-			OutputStreamWriter out = new OutputStreamWriter(
-			    httpCon.getOutputStream());
-			out.write(json);
-			out.close();
-			httpCon.getInputStream();
+		String relatedID = aPlayer.getFeedmonster();
+		Player tPlayer = map.get(relatedID);
+		if (tPlayer == null) {
+			logger.info("new player detected : " + aPlayer.getName());
+			submit(getJson(aPlayer), null);
+		} else {
+			submit(getJson(aPlayer), tPlayer.getId());
 		}
 	}
 
-	
+	private String getJson(Player aPlayer) {
+		JSONObject jsono = new JSONObject();
+
+		try {
+			jsono.put("comunio", aPlayer.getComunio());
+			jsono.put("feedmonster", aPlayer.getFeedmonster());
+			jsono.put("country", aPlayer.getCountry());
+			jsono.put("firstName", aPlayer.getFirstName());
+			jsono.put("lastName", aPlayer.getLastName());
+			jsono.put("name", aPlayer.getName());
+			jsono.put("abbreviationName", aPlayer.getAbbreviationName());
+			jsono.put("position", aPlayer.getPosition());
+			jsono.put("age", aPlayer.getAge());
+			jsono.put("thumbnail", aPlayer.getThumbnail());
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return jsono.toString();
+	}
+
+	void submit(String json, String id) throws MalformedURLException, IOException, ProtocolException {
+		String endpoint = "http://fussballmanager.herokuapp.com/Player/";
+		if (id == null) {
+			endpoint += "new";
+		} else {
+			endpoint += id;
+		}
+		URL url = new URL(endpoint);
+		HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+		httpCon.setDoOutput(true);
+		httpCon.setRequestMethod("PUT");
+		OutputStreamWriter out = new OutputStreamWriter(httpCon.getOutputStream());
+		out.write(json);
+		out.close();
+		httpCon.getInputStream();
+	}
+
 	private String loadFile(String url) {
 
 		StringBuffer tempReturn = new StringBuffer();
@@ -95,13 +120,11 @@ public class PlayerService  {
 			}
 
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return tempReturn.toString();
 	}
-	
+
 }
